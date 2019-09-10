e106b661a5ba638acf2761aa1659d80041f84f53
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-09-09 21:40:00 -0400

    useDarkMode will take in an initial value, and set the value in localStorage to this initialValue. The useEffect hook will change the classList of the body tag.

--- src/utility/useDarkMode.js
+++ src/utility/useDarkMode.js
@@ -1,15 +1,15 @@
 import { useEffect } from "react";
 import useLocalStorage from "./useLocalStorage";
 
-const useDarkMode = element => {
-  const [darkMode, setDarkMode] = useLocalStorage("darkMode", false);
+const useDarkMode = initialValue => {
+  const [darkMode, setDarkMode] = useLocalStorage("darkMode", initialValue);
   useEffect(() => {
     if (darkMode) {
       // add the darkmode class to the body element
-      element.classList.add("dark-mode");
+      document.body.classList.add("dark-mode");
     } else {
       // Remove the darkmode class from the body element
-      element.classList.remove("dark-mode");
+      document.body.classList.remove("dark-mode");
     }
   }, [darkMode]);
   return [darkMode, setDarkMode];
