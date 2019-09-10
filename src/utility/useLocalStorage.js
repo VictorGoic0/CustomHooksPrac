cd994180a96195eda643a7d7391940f3093eea7a
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-09-09 21:15:18 -0400

    Set the default value of the storedValue state to the initialValue if the key had not been saved to localStorage before. Else, set it to the item in localStorage, using JSON.parse() on it to convert it from its string format.

--- src/utility/useLocalStorage.js
+++ src/utility/useLocalStorage.js
@@ -1,5 +1,10 @@
 import { useState } from "react";
 
-const useLocalStorage = (key, initialValue) => {};
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+};
 
 export default useLocalStorage;
