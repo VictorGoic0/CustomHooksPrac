0908fe371927739512e8c4460ceeba222d616caf
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-09-09 21:19:13 -0400

    Added a function within the useLocalStorage hook, setValue, which will adjust the storedValue state and then set it to localStorage, using the same key in the parameter of the useLocalStorage hook.

--- src/utility/useLocalStorage.js
+++ src/utility/useLocalStorage.js
@@ -5,6 +5,12 @@ const useLocalStorage = (key, initialValue) => {
     const item = localStorage.getItem(key);
     return item ? JSON.parse(item) : initialValue;
   });
+
+  const setValue = value => {
+    setStoredValue(value);
+    localStorage.setItem(key, JSON.stringify(value));
+  };
+  return [storedValue, setValue];
 };
 
 export default useLocalStorage;
