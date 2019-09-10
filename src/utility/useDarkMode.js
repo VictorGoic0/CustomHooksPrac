4de3049285531e74d92e647ee7be9c0a11e6d7b2
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-09-09 21:32:21 -0400

    The useEffect hook within the useDarkMode hook will check if darkMode is set to true or false each time it is updated. If true, it will add dark-mode to the classList of the given element. If false, it will remove the dark-mode class. Finally, the useDarkMode hook will return the darkMode and setDarkMode variables that were returned by the useLocalStorage hook, allowing the useDarkMode hook to return the state of darkMode and the function for setting it.

--- src/utility/useDarkMode.js
+++ src/utility/useDarkMode.js
@@ -1,15 +1,18 @@
 import { useEffect } from "react";
 import useLocalStorage from "./useLocalStorage";
 
-const useDarkMode = boolean => {
-  const [darkMode, setDarkMode] = useLocalStorage("darkMode", boolean);
+const useDarkMode = element => {
+  const [darkMode, setDarkMode] = useLocalStorage("darkMode", false);
   useEffect(() => {
     if (darkMode) {
       // add the darkmode class to the body element
+      element.classList.add("dark-mode");
     } else {
-      // Remove it
+      // Remove the darkmode class from the body element
+      element.classList.remove("dark-mode");
     }
   }, [darkMode]);
+  return [darkMode, setDarkMode];
 };
 
 export default useDarkMode;
